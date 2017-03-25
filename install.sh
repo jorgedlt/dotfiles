fc8c97533bfa24ca0a3c152c63213aea75e6cf55
Jorge_de_la_Torre <jorgedlt@gmail.com>
2017-03-24 21:33:00 -0400

    Commit message

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# install.sh
+
+# add archive of preexsiting files
+
+# my bashrc
+/bin/cp -f ./dot.bashrc ~/.bashrc
+
+# my functions
+/bin/cp -f ./dot.bash-func.sh ~/.bash-func.sh
+
+#
---
+++ insync.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# insync.sh
+
+# add archive of preexsiting files
+
+# my bashrc
+diff ./dot.bashrc ~/.bashrc
+
+# my functions
+diff ./dot.bash-func.sh ~/.bash-func.sh
+
+#
