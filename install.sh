b14d83a3abf28959975b17c28d626f6c9d17c293
Jorge_de_la_Torre <jorgedlt@gmail.com>
2017-03-24 21:52:12 -0400

    Commit message

--- install.sh
+++ install.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
-# install.sh
+# install.sh - for dot.files
 
 # add archive of preexsiting files
+echo; echo "${GREEN}INSTALLATION SCRIPT - $(git remote get-url origin) ${YELLOW}"
 
+echo
 # my bashrc
+echo ${GREEN}installing ... ~/.bashrc${RESET}
 /bin/cp -f ./dot.bashrc ~/.bashrc
+#ls -la ./dot.bashrc ~/.bashrc
 
 # my functions
+echo ${GREEN}installing ... ~/.bash-func.sh${RESET}
 /bin/cp -f ./dot.bash-func.sh ~/.bash-func.sh
+#ls -la ./dot.bash-func.sh ~/.bash-func.sh
 
 #
+echo; echo ${GREEN}comparing ... ~/.bash-func.sh${RESET}
+./insync.sh
--- insync.sh
+++ insync.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-# insync.sh
-
-# add archive of preexsiting files
+# insync.sh - for dot.files
 
 # my bashrc
-diff ./dot.bashrc ~/.bashrc
+echo; echo ${GREEN}~/.bashrc ${YELLOW}
+diff -sb ./dot.bashrc ~/.bashrc
+echo ${RESET}
+ls -la ./dot.bashrc ~/.bashrc
 
 # my functions
-diff ./dot.bash-func.sh ~/.bash-func.sh
+echo; echo ${GREEN} ~/.bash-func.sh ${YELLOW}
+diff -sb ./dot.bash-func.sh ~/.bash-func.sh
+echo ${RESET}
+ls -la ./dot.bash-func.sh ~/.bash-func.sh
 
 #
